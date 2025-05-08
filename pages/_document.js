9929e72a2825a238d6cce1c75c385cead6aba4a6
Mohsin Khan <209888552+BayHatter@users.noreply.github.com>
2025-05-08 06:59:09 +0500

    Update _document.js

--- pages/_document.js
+++ pages/_document.js
@@ -4,9 +4,9 @@ export default function Document() {
   return (
     <Html>
       <Head>
-        <title>Ethereum Token Creation Tool</title>
-        <meta name="description" content="Create and launch your own meme token on Ethereum" />
-        <meta property="og:title" content="Launch Your Meme Coin on Ethereum" />
+        <title>WEMIX3.0 Token Creation Tool</title>
+        <meta name="description" content="Create and launch your own meme token on WEMIX3.0" />
+        <meta property="og:title" content="Launch Your Meme Coin on WEMIX3.0" />
         <meta property="og:description" content="Build and deploy a meme coin instantly. Fast, secure." />
         <meta property="og:url" content="/" />
         <meta property="og:type" content="website" />
